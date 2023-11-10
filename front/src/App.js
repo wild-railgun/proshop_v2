ea29cdc6abd6e1178e6f5e3bb2d49c1b8f6a9e75
wild-railgun <wildrailgun@gmail.com>
2023-11-10 19:37:04 +0500

    Added Header Component and Some Other Stuff

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
+import Header from "./components/Header"
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>Welcome to ProShop</h1>
+        </Container>
+      </main>
+    </>
   )
 }
 
---
+++ front/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+
+const Footer = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Footer
