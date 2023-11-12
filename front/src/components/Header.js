4c6f31b1d3cb68c1f218a2fc5c956852af48ebd7
wild-railgun <wildrailgun@gmail.com>
2023-11-12 17:39:54 +0500

    Replaced Anchor Tags with Links from react-router-dom

--- front/src/components/Header.js
+++ front/src/components/Header.js
@@ -1,20 +1,29 @@
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
 import logo from '../assets/logo.png';
+import { LinkContainer } from 'react-router-bootstrap';
 
 const Header = () => {
     return (
         <header>
             <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
                 <Container>
-                    <Navbar.Brand href="/">
-                        <img src={logo} alt="ProShop" />
-                        Proshop</Navbar.Brand>
+                    <LinkContainer to='/'>
+                        <Navbar.Brand>ProShop</Navbar.Brand>
+                    </LinkContainer>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="ms-auto">
-                            <Nav.Link href="/cart"><FaShoppingCart /> Cart</Nav.Link>
-                            <Nav.Link href="/login"><FaUser/> Sign In</Nav.Link>
+                            <LinkContainer to='/cart'>
+                                <Nav.Link>
+                                    <FaShoppingCart /> Cart
+                                </Nav.Link>
+                            </LinkContainer>
+                            <LinkContainer to='/login'>
+                                <Nav.Link href='/login'>
+                                    <FaUser /> Sign In
+                                </Nav.Link>
+                            </LinkContainer>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
