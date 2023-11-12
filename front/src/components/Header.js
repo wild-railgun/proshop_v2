a0e6cea766505f54e29003a0409b570d432b7a2a
wild-railgun <wildrailgun@gmail.com>
2023-11-12 15:00:49 +0500

    Add Custom Styles

--- front/src/components/Header.js
+++ front/src/components/Header.js
@@ -1,17 +1,20 @@
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
+import logo from '../assets/logo.png';
 
 const Header = () => {
     return (
         <header>
             <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
                 <Container>
-                    <Navbar.Brand href="/">Proshop</Navbar.Brand>
+                    <Navbar.Brand href="/">
+                        <img src={logo} alt="ProShop" />
+                        Proshop</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="ms-auto">
                             <Nav.Link href="/cart"><FaShoppingCart /> Cart</Nav.Link>
-                            <Nav.Link href="/login"><FaShoppingCart /> Sign In</Nav.Link>
+                            <Nav.Link href="/login"><FaUser/> Sign In</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
