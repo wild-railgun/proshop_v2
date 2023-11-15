6e5bd8fafa345214c532eccf90dc975dc9b401ba
wild-railgun <wildrailgun@gmail.com>
2023-11-15 17:58:41 +0500

    Added error middleware

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+
 const port = process.env.PORT || 5000;
 
 connectDB();
@@ -15,4 +17,9 @@ app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.use(notFound);
+app.use(errorHandler);
+
+app.listen(port, () =>
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)
+ );
