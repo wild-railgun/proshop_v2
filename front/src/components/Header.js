bd4c566b3befba0c106468d86fecdc9f3de4e280
wild-railgun <wildrailgun@gmail.com>
2023-11-15 14:31:12 +0500

    Add some stuff

--- front/src/components/Header.js
+++ front/src/components/Header.js
@@ -9,7 +9,10 @@ const Header = () => {
             <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
                 <Container>
                     <LinkContainer to='/'>
-                        <Navbar.Brand>ProShop</Navbar.Brand>
+                        <Navbar.Brand>
+                            <img src={logo} alt="ProShop" />
+                            ProShop
+                        </Navbar.Brand>
                     </LinkContainer>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
