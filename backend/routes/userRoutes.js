4831a4a0946529ed9bf931fc949d42a20aaf4a2d
wild-railgun <wildrailgun@gmail.com>
2023-11-26 21:29:36 +0500

    User Authentication Endpoint

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -14,7 +14,7 @@ import {
 const router = express.Router();
 
 router.route('/').post(registerUser).get(getUsers);
-router.post('/login', authUser);
+router.post('/auth', authUser);
 router.route('/profile').get(getUserProfile).put(updateUserProfile);
 
 router.route('/:id').delete(deleteUser).get(getUserById).put(updateUser);
