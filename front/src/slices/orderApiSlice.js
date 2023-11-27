8f7aab15b650783bb069cd6649b32fd0116769cb
wild-railgun <wildrailgun@gmail.com>
2023-11-27 22:27:30 +0500

    Admin Order List

--- front/src/slices/orderApiSlice.js
+++ front/src/slices/orderApiSlice.js
@@ -35,7 +35,20 @@ export const orderApiSlice = apiSlice.injectEndpoints({
       }),
       keepUnusedDataFor: 5,
     }),
+    getOrders: builder.query({
+      query: () => ({
+        url: ORDERS_URL,
+      }),
+      keepUnusedDataFor: 5,
+    }),
   }),
 });
 
-export const { useCreateOrderMutation, useGetOrderDetailsQuery, usePayOrderMutation, useGetPaypalClientIdQuery, useGetMyOrdersQuery } = orderApiSlice;
+export const { 
+  useCreateOrderMutation, 
+  useGetOrderDetailsQuery, 
+  usePayOrderMutation, 
+  useGetPaypalClientIdQuery, 
+  useGetMyOrdersQuery,
+  useGetOrdersQuery
+} = orderApiSlice;
