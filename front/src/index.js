f36a560e0cb8dfa6fd7c50555588e22438e42eaf
wild-railgun <wildrailgun@gmail.com>
2023-11-28 20:44:40 +0500

    Product Pagination and Paginate Component

--- front/src/index.js
+++ front/src/index.js
@@ -37,6 +37,7 @@ const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App />}>
       <Route index={true} path='/' element={<HomeScreen />} />
+      <Route path='/page/:pageNumber' element={<HomeScreen />} />
       <Route path='/product/:id' element={<ProductScreen />} />
       <Route path='/cart' element={<CartScreen />} />
       <Route path='/login' element={<LoginScreen />} />
@@ -52,6 +53,10 @@ const router = createBrowserRouter(
       {/* Admin users */}
       <Route path='' element={<AdminRoute />}>
         <Route path='/admin/orderlist' element={<OrderListScreen />} />
+        <Route
+          path='/admin/productlist/:pageNumber'
+          element={<ProductListScreen />}
+        />
         <Route path='/admin/productlist' element={<ProductListScreen />} />
         <Route path='/admin/userlist' element={<UserListScreen />} />
         <Route path='/admin/product/:id/edit' element={<ProductEditScreen />} />
--- front/src/screens/HomeScreen.js
+++ front/src/screens/HomeScreen.js
@@ -1,11 +1,19 @@
 import { useGetProductsQuery } from '../slices/productsApiSlice';
+import { useParams } from 'react-router-dom';
 import { Row, Col } from 'react-bootstrap';
 import Product from '../components/Product';
 import Loader from '../components/Loader';
 import Message from '../components/Message';
+import Paginate from '../components/Paginate';
 
 const HomeScreen = () => {
-  const { data: products, isLoading, error } = useGetProductsQuery();
+  const { pageNumber } = useParams();
+
+  console.log(pageNumber);
+
+  const { data, isLoading, error } = useGetProductsQuery({
+    pageNumber,
+  });
 
   return (
     <>
@@ -18,12 +26,13 @@ const HomeScreen = () => {
         <>
           <h1>Latest Products</h1>
           <Row>
-            {products.map((product) => (
+            {data.products.map((product) => (
               <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
                 <Product product={product} />
               </Col>
             ))}
           </Row>
+          <Paginate pages={data.pages} page={data.page} />
         </>
       )}
     </>
